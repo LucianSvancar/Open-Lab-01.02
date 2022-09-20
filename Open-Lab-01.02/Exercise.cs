880f79b449fceefee8aedb0879be2b15e2354e8f
LucianSvancar <svancarlucian@gmail.com>
2022-09-20 09:50:06 +0200

    Add returt a + b it . Means you make sum of( int a) and (int b)

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -1,39 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PROJEKTIK
+namespace Open_Lab_01._02
 {
-    internal class Program
+    public class Exercise
     {
-        static void Main(string[] args)
+        public int Sum(int a, int b)
         {
-
-            Console.Write("Napiš čislo: ");
-            int čislo1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Napiš 2 čislo: ");
-            int čislo2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Výsledok je: ");
-
-            Console.WriteLine(čislo1 + čislo2);
-
-
-
-
-
-
-
-
-
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
+           return  a +  b;
         }
     }
 }
